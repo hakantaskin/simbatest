f520726debf7cbe3145dc6352bea8e72248b7229
hakantaskin <mr.hakantaskin@gmail.com>
2016-07-29 15:00:56 +0300

    second commit

--- app/background.js
+++ app/background.js
@@ -2,8 +2,8 @@
 // app starts. This script is running through entire life of your application.
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
-
-import { app, Menu, ipcMain } from 'electron';
+import os from 'os';
+import { app, Menu, ipcMain, autoUpdater, dialog } from 'electron';
 import { devMenuTemplate } from './helpers/dev_menu_template';
 import { editMenuTemplate } from './helpers/edit_menu_template';
 import createWindow from './helpers/window';
@@ -12,31 +12,7 @@ import createWindow from './helpers/window';
  // module loaded from npm
 import env from './env';
 import site from './site';
-const GhReleases = require('electron-gh-releases');
-
-let options = {
-  repo: 'hakantaskin/simbatest',
-  currentVersion: app.getVersion();
-}
-const updater = new GhReleases(options);
-
-
-updater.check((err, status) => {
-  if (!err && status) {
-    // Download the update
-    updater.download()
-  }
-})
-
-// When an update has been downloaded
-updater.on('update-downloaded', (info) => {
-  // Restart the app and install the update
-  updater.install()
-})
-
-// Access electrons autoUpdater
-updater.autoUpdater
-
+console.log(app.getPath('appData'));
 var mainWindow;
 var setApplicationMenu = function () {
     var menus = [editMenuTemplate];
@@ -46,6 +22,33 @@ var setApplicationMenu = function () {
     Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
 };
 
+var auto_update = function (mainWindow) {
+  //set api url
+  var feedUrl = 'http://localhost:3000/update/' + os.platform() + '?version=' + app.getVersion();
+  console.log(feedUrl);
+  autoUpdater.setFeedURL(feedUrl);
+
+  //event handling after download new release
+  autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+
+    //confirm install or not to user
+    var index = dialog.showMessageBox(mainWindow, {
+      type: 'info',
+      buttons: [i18n.__('Restart'), i18n.__('Later')],
+      title: "Typetalk",
+      message: i18n.__('The new version has been downloaded. Please restart the application to apply the updates.'),
+      detail: releaseName + "\n\n" + releaseNotes
+    });
+
+    if (index === 1) {
+      return;
+    }
+
+    //restart app, then update will be applied
+    autoUpdater.quitAndInstall();
+  });
+};
+
 app.on('ready', function () {
     setApplicationMenu();
     var url = 'file://' + __dirname + '/app.html';
@@ -53,10 +56,13 @@ app.on('ready', function () {
     var options = {
       width: 1000,
       height:600,
-      show:false
+      show:true
     }
 
     var mainWindow = createWindow('main', options);
+    if (os.platform() != 'darwin') {
+        auto_update(mainWindow);
+    }
     var settings_options = {
       width: 350,
       height:150,
