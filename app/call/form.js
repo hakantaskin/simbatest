b3f0509080eef8e0475470b8f892af06fe8ed866
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 18:14:01 +0300

    winnnn

--- app/call/form.js
+++ app/call/form.js
@@ -6,13 +6,14 @@
     var { ipcRenderer } = require('electron');
     //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
     ipcRenderer.on('windowname', (event, arg) => {
-      console.log("renderer");
       var url = arg[1];
       var caller_id = arg[2];
       var website = arg[3];
       var agent = arg[4];
-      $('.tab-div').html('<webview id="1" src="' + url + '" style="display:inline-flex; width:640px; height:480px"></webview>');
-      $('.phone_number').html('Phone Number :' + caller_id);
+      $(document).ready(function(){
+        $('.tab-div').html('<webview id="1" src="' + url + '" style="display:inline-flex; width:640px; height:480px"></webview>');
+        $('.phone_number').html('Phone Number :' + caller_id);
+      });
     });
 
 }());
