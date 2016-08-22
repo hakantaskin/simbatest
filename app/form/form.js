1a084b166d374565d3e0307a77b6299cde59ff9a
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 17:21:44 +0300

    winnn

--- app/form/form.js
+++ app/form/form.js
@@ -1,9 +1,9 @@
 (function () {
   'use strict';
 
-  const electron = require('electron');
-  const ipcMain = electron.ipcMain;
-  const ipcRenderer = electron.ipcRenderer;
+  var config = require('./../stylesheets/js/config');
+  var remote = require('electron').remote;
+  var { ipcMain, ipcRenderer } = require('electron');
   //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
   ipcMain.on('windowname', (event, arg) => {
     console.log("main");
