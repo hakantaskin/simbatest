9fba07ef76919e41379b23bd39de396a5053763e
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 15:55:03 +0300

    winn

--- app/form.js
+++ app/form.js
@@ -1,6 +1,13 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, ipcMain } = require('electron');
+
+//arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
+ipcMain.on('windowname', (event, arg) => {
+  console.log("main");
+  console.log(arg);
+}
 
 //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
 ipcRenderer.on('windowname', (event, arg) => {
+  console.log("renderer");
   console.log(arg);
 }
