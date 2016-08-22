7088f3603ed7fcb3f8ca85328304f9b215d886d6
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 16:49:03 +0300

    winnn test

--- app/form.js
+++ app/form.js
@@ -1,7 +1,13 @@
-const { ipcMain } = require('electron');
+const { ipcMain, ipcRenderer } = require('electron');
 
 //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
 ipcMain.on('windowname', (event, arg) => {
   console.log("main");
   console.log(arg);
-}
+});
+
+//arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
+ipcRenderer.on('windowname', (event, arg) => {
+  console.log("renderer");
+  console.log(arg);
+});
