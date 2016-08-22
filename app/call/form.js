9ff0d41fa01ff2d525d9e6d3706cee9d352e2285
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 18:10:47 +0300

    win

--- app/call/form.js
+++ app/call/form.js
@@ -2,11 +2,17 @@
 // All stuff below is just to show you how it works. You can delete all of it.
 (function () {
     'use strict';
+    var config = require('./../stylesheets/js/config');
     var { ipcRenderer } = require('electron');
     //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
     ipcRenderer.on('windowname', (event, arg) => {
       console.log("renderer");
-      console.log(arg);
+      var url = arg[1];
+      var caller_id = arg[2];
+      var website = arg[3];
+      var agent = arg[4];
+      $('.tab-div').html('<webview id="1" src="' + url + '" style="display:inline-flex; width:640px; height:480px"></webview>');
+      $('.phone_number').html('Phone Number :' + caller_id);
     });
 
 }());
