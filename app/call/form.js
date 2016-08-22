31f211c5a379c386bdb109ae7a49e0e07910e480
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 17:46:41 +0300

    winn form

--- app/call/form.js
+++ app/call/form.js
@@ -2,13 +2,7 @@
 // All stuff below is just to show you how it works. You can delete all of it.
 (function () {
     'use strict';
-    var { ipcMain, ipcRenderer } = require('electron');
-    //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
-    ipcMain.on('windowname', (event, arg) => {
-      console.log("main");
-      console.log(arg);
-    });
-
+    var { ipcRenderer } = require('electron');
     //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
     ipcRenderer.on('windowname', (event, arg) => {
       console.log("renderer");
