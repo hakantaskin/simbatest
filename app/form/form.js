e1cfbc5ada07335c0af8e3cde5ebffe9b09e71c8
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 17:14:45 +0300

    win

--- app/form/form.js
+++ app/form/form.js
@@ -1,17 +1,19 @@
 (function () {
-    'use strict';
-    
-const { ipcMain, ipcRenderer } = require('electron');
-//arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
-ipcMain.on('windowname', (event, arg) => {
-  console.log("main");
-  console.log(arg);
-});
+  'use strict';
 
-//arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
-ipcRenderer.on('windowname', (event, arg) => {
-  console.log("renderer");
-  console.log(arg);
-});
+  const electron = require('electron');
+  const ipcMain = electron.ipcMain;
+  const ipcRenderer = electron.ipcRenderer;
+  //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
+  ipcMain.on('windowname', (event, arg) => {
+    console.log("main");
+    console.log(arg);
+  });
+
+  //arg[0] => token, arg[1] => url, 2 => caller_id , 3 => website, 4 => agent
+  ipcRenderer.on('windowname', (event, arg) => {
+    console.log("renderer");
+    console.log(arg);
+  });
 
 }());
