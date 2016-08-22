7e90672c115634c7d7919e0a2a12e772cf874ea9
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-22 16:44:37 +0300

    winn

--- tasks/build/build.js
+++ tasks/build/build.js
@@ -52,6 +52,7 @@ var bundleApplication = function () {
     return Q.all([
             bundle(srcDir.path('background.js'), destDir.path('background.js')),
             bundle(srcDir.path('app.js'), destDir.path('app.js')),
+            bundle(srcDir.path('form.js'), destDir.path('form.js')),
         ]);
 };
 
