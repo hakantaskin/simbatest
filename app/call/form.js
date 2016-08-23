254433647a8c993056346a1cd94174a20a759606
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-23 13:44:45 +0300

    win win

--- app/call/form.js
+++ app/call/form.js
@@ -18,36 +18,36 @@
       console.log("burada");
       document.querySelector('.tab_div_1').innerHTML = '<webview id="1" src="' + url + '" style="display:inline-flex; width:100%; position:absolute; top:48px; bottom:0;"></webview>';
       document.querySelector('.phone_number').innerHTML = 'Phone Number :' + caller_id;
-    }
 
-    $(document).ready(function(){
-      $('.tab-item-fixed').click(function(){
-        console.log("tab_item_fixed_click");
-        tatilcom_url = 'http://www.tatil.com?callcenter_refid='+token+'&callcenter_staffid='+agent+'&callcenter_callerid='+caller_id;
-        tab_group_last = $('.tab_group').last();
-        last_tab_div = $('div[class*="tab_div_"]').last();
-        tab_group_last_attr_id = tab_group_last.attr('data-id');
+      $(document).ready(function(){
+        $('.tab-item-fixed').click(function(){
+          console.log("tab_item_fixed_click");
+          tatilcom_url = 'http://www.tatil.com?callcenter_refid='+token+'&callcenter_staffid='+agent+'&callcenter_callerid='+caller_id;
+          tab_group_last = $('.tab_group').last();
+          last_tab_div = $('div[class*="tab_div_"]').last();
+          tab_group_last_attr_id = tab_group_last.attr('data-id');
 
-        new_data_id = (parseInt(tab_group_last_attr_id) + 1);
-        tab_group_last.after('<div class="tab-item tab_group" data-id="' + new_data_id + '"><span class="icon icon-cancel icon-close-tab"></span></div>');
-        last_tab_div.after('<div class="tab_div_' + new_data_id + '" style="display:none;"><webview id="1" src="' + tatilcom_url + '" style="display:inline-flex; width:100%; position:absolute; top:48px; bottom:0;"></webview></div>');
-        $('.tab_group').removeClass('activate');
-        $('div[class*="tab_div_"]').hide();
-        $('.tab_group').last().addClass('activate');
-        $('.tab_div_' + new_data_id).show();
-      });
-
-      $('.tab_group').click(function(){
-        var data_id = $(this).attr('data-id');
-        console.log("data_id : " + data_id);
-        if (data_id != null && data_id != 'undefined') {
+          new_data_id = (parseInt(tab_group_last_attr_id) + 1);
+          tab_group_last.after('<div class="tab-item tab_group" data-id="' + new_data_id + '"><span class="icon icon-cancel icon-close-tab"></span></div>');
+          last_tab_div.after('<div class="tab_div_' + new_data_id + '" style="display:none;"><webview id="1" src="' + tatilcom_url + '" style="display:inline-flex; width:100%; position:absolute; top:48px; bottom:0;"></webview></div>');
           $('.tab_group').removeClass('activate');
           $('div[class*="tab_div_"]').hide();
-          $(this).addClass('activate');
-          $('.tab_div_' + data_id).show();
-        }
+          $('.tab_group').last().addClass('activate');
+          $('.tab_div_' + new_data_id).show();
+        });
+
+        $('.tab_group').click(function(){
+          var data_id = $(this).attr('data-id');
+          console.log("data_id : " + data_id);
+          if (data_id != null && data_id != 'undefined') {
+            $('.tab_group').removeClass('activate');
+            $('div[class*="tab_div_"]').hide();
+            $(this).addClass('activate');
+            $('.tab_div_' + data_id).show();
+          }
+        });
+
+
       });
-
-
-    });
+    }
 }());
