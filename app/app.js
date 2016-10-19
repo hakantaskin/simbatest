0bb8dcc2f3d49498b11538646169e817aeb5ede9
hakantaskin <mr.hakantaskin@gmail.com>
2016-10-19 18:53:52 +0300

    master

--- app/app.js
+++ app/app.js
@@ -110,7 +110,8 @@ var notifier_api = function(funct_token, func_window, new_connection_id) {
   caller_id = get_caller_id(new_connection_id);
   last_direction = get_last_direction();
   if(last_direction != ''){
-      data[new_connection_id] = {"last_direction": true};
+      data[new_connection_id].last_direction = true};
+      console.log("conn id : " + new_connection_id + "last_direction = true");
   }
 
   if(funct_token == ''){
@@ -153,7 +154,8 @@ var notifier_api = function(funct_token, func_window, new_connection_id) {
           height:600
         }
         if(caller_id.length > 5 && caller_id.indexOf('*') == -1){
-          data[new_connection_id] = {"caller_id": true};
+          data[new_connection_id].caller_id = true;
+          console.log("conn id : " + new_connection_id + "last_direction = true");
           var win2 = ipcRenderer.send('newwindow', [funct_token, screen_temp_url, caller_id, website, user_name]);
         }
         // Create a new window
