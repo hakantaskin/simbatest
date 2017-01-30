aae8b9d85055a8d945d994ee72b0d23c9d3ed73b
hakantaskin <mr.hakantaskin@gmail.com>
2017-01-30 17:34:20 +0300

    altta

--- app/app.js
+++ app/app.js
@@ -312,8 +312,8 @@ var watch_file_2 = function(){
   website = get_website(site);
   timestamp = new Date().getTime();
   tail.on("line", function(tail_data) {
-    append_log_file(11, tail_data);
     new_conn_id = get_connection_id_by_data(tail_data);
+    append_log_file(11, tail_data);
     /*
     if(last_conn_id != '' && last_conn_id != -1 && new_conn_id != -1 && new_conn_id != ''){
       if(new_conn_id != last_conn_id){
